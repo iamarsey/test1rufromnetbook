31a5f5744fa1ed67ac4e562a752fd24bfc46d3d2
Artem <xxarseyxx@gmail.com>
2013-04-15 00:40:34 +0400

    Добавление файлов

---
+++ www/test.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: MountainArs
+ * Date: 14.04.13
+ * Time: 15:52
+ * To change this template use File | Settings | File Templates.
+ */
+echo "play";
